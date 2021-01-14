d2c068031e6fcc3aa49818a25fc8c3251d2b40cd
Graham Cox <graham@grahamcox.co.uk>
2021-01-14 07:34:43 +0000

    Dotenv

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,7 +1,11 @@
 #![deny(clippy::all, clippy::pedantic, clippy::nursery)]
 
+use dotenv::dotenv;
+
 #[actix_rt::main]
 async fn main() {
+    dotenv().ok();
+
     tracing_subscriber::fmt::init();
 
     patina::main().await;
