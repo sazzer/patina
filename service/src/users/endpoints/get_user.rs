8f4e81ad67f4adcf083fc58914e8bae2e133a54a
Graham Cox <graham@grahamcox.co.uk>
2021-01-17 12:20:53 +0000

    Actually get the user in the endpoint, returning a 404 if the User ID was invalid or the user wasn't found

--- service/src/users/endpoints/get_user.rs
+++ service/src/users/endpoints/get_user.rs
@@ -1,13 +1,20 @@
 use std::sync::Arc;
 
-use actix_web::web::Data;
+use actix_web::web::{Data, Path};
 
 use crate::{
     http::problem::{Problem, NOT_FOUND},
-    users::GetUserUseCase,
+    users::{GetUserUseCase, UserID},
 };
 
 /// Endpoint for getting a single User by ID.
-pub async fn get_user(_health_service: Data<Arc<dyn GetUserUseCase>>) -> Result<String, Problem> {
+pub async fn get_user(path: Path<String>, user_service: Data<Arc<dyn GetUserUseCase>>) -> Result<String, Problem> {
+    let user_id: UserID = path.0.parse().map_err(|e| {
+        tracing::warn!(e = ?e, user_id = ?path.0, "Failed to parse User ID");
+        Problem::new(NOT_FOUND)
+    })?;
+
+    let _user = user_service.get_user_by_id(user_id).await.ok_or_else(|| Problem::new(NOT_FOUND))?;
+
     Err(NOT_FOUND.into())
 }
